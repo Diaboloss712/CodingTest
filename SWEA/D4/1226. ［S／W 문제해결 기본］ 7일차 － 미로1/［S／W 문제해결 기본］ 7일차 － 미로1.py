18429b3fc842d06540a49320a522ab4d93955e30
Diaboloss712 <diaboloss217+github@gmail.com>
2025-02-20 11:43:15 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 77 ms, Memory: 65,408 KB -BaekjoonHub

---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -0,0 +1,41 @@
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+
+def bfs(start):
+    global board
+    global answer
+    visited = [[0] * 16 for _ in range(16)]
+    queue = [start]
+    while queue:
+        cur_pos = queue.pop()
+        cur_x = cur_pos[0]
+        cur_y = cur_pos[1]
+        for direction in range(4):
+            nx = cur_x + dx[direction]
+            ny = cur_y + dy[direction]
+            if nx >= 16 or ny >= 16 or nx < 0 or ny < 0:
+                continue
+            if visited[ny][nx]:
+                continue
+            if board[ny][nx] == 0:
+                visited[ny][nx] = 1
+                queue.append([nx, ny])
+            elif board[ny][nx] == 3:
+                answer = 1
+                return
+
+
+for test_case in range(1, 11):
+    T = int(input())
+    answer = 0
+    board = [[0] * 16 for _ in range(16)]
+    for i in range(16):
+        board[i] = list(map(int, input()))
+    for i in range(16):
+        for j in range(16):
+            if board[i][j] == 2:
+                start_x = j
+                start_y = i
+    bfs([start_x, start_y])
+    print(f'#{test_case} {answer}')
